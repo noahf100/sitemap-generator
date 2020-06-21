cdb772eeb9c2df163bec5076c8de20ad3ad3c614
Noah  Fang <NoahFang@Noahs-MBP-6.fios-router.home>
2020-06-21 14:25:48 -0400

    correctness fixes

--- pysitemap/__init__.py
+++ pysitemap/__init__.py
@@ -17,8 +17,11 @@ def crawler(root_url, out_file, out_format='xml', maxtasks=100, batch_size=10000
     loop = asyncio.get_event_loop()
 
     # TODO (noahfang) - put name in assets file and use here and in base_crawler.py
-    os.remove('seen.db')
-    os.remove('todo.db')
+    try:
+      os.remove('seen.sqlite')
+      os.remove('todo.sqlite')
+    except:
+      pass
 
     c = Crawler(root_url, out_file=out_file, out_format=out_format, maxtasks=maxtasks, batch_size=batch_size, prefix=prefix)
     loop.run_until_complete(c.run())
@@ -27,7 +30,5 @@ def crawler(root_url, out_file, out_format='xml', maxtasks=100, batch_size=10000
         loop.add_signal_handler(signal.SIGINT, loop.stop)
     except RuntimeError:
         pass
-    print('todo_queue:', len(c.todo_queue))
     print('busy:', len(c.busy))
     print('done:', len(c.done), '; ok:', sum(c.done.values()))
-    print('tasks:', len(c.tasks))
